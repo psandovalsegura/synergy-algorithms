89867d58171d7be69675c55f4a0e4d54b150f395
Pedro Sandoval <psandovalsegura@gmail.com>
2020-03-21 18:00:40 -0400

    Add weighted random graph neighbor

--- src/weighted_synergy_graph.py
+++ src/weighted_synergy_graph.py
@@ -1,3 +1,5 @@
+import copy
+import random
 import networkx as nx
 from src.synergy_graph import SynergyGraph
 
@@ -31,3 +33,48 @@ class WeightedSynergyGraph(SynergyGraph):
 		else:
 			return nx.algorithms.bellman_ford_path_length(self.graph, a, b)
 
+def random_weighted_graph_neighbor(G):
+	"""
+	G is a networkx graph with self-loops and weighted edges
+
+	either increases the weight of a random edge by 1 (subject to a max of w_max), 
+	or decreases the weight of a random edge by 1 (subject to a min of w_min),
+	or removes an existing edge, subject to the constraint that G remains connected,
+	or adds a new random edge
+	"""
+	H = copy.deepcopy(G)
+	edges = [e for e in H.edges]
+	nodes = [n for n in H]
+
+	while True:
+		rand = random.random()
+		if rand < 0.25:
+			# Increase weight of random edge by 1
+			increase_edge = random.choice(edges)
+			H[increase_edge[0]][increase_edge[1]]['weight'] += 1
+		elif rand < 0.5:
+			# Decrease weight of random edge by 1
+			decrease_edge = random.choice(edges)
+			H[decrease_edge[0]][decrease_edge[1]]['weight'] -= 1
+		elif rand < 0.75:
+			# if there are no edges to remove, do nothing
+			if len(edges) == 0:
+				break
+			removal_edge = random.choice(edges)
+			H.remove_edge(*removal_edge)
+		else:
+			potential_new_edges = set(itertools.combinations(nodes, r=2)) - set(edges)
+			# if there are no new edges to add, do nothing
+			if len(potential_new_edges) == 0:
+				break
+			new_edge = random.choice(list(potential_new_edges))
+			H.add_edge(*new_edge)
+
+		# if the graph becomes disconnected we
+		# should add back the edge which was removed
+		if nx.is_connected(H):
+			break
+		else:
+			H.add_edge(*removal_edge)
+
+	return H
--- tests/test_weighted_synergy_graph.py
+++ tests/test_weighted_synergy_graph.py
@@ -67,6 +67,9 @@ def test_get_distance_3():
 	assert WS.get_distance(0, 1) == 2
 
 def test_get_capability():
+	"""
+	check that the correct distribution is returned
+	"""
 	WS = get_figure_2_weighted_synergy_graph()
 	assert WS.get_capability(0, 0) == NormalDistribution(1,1)
 	assert WS.get_capability(0, 1) == NormalDistribution(1,2)
@@ -74,3 +77,29 @@ def test_get_capability():
 	assert WS.get_capability(1, 1) == NormalDistribution(2,2)
 	assert WS.get_capability(2, 0) == NormalDistribution(3,1)
 	assert WS.get_capability(2, 1) == NormalDistribution(3,2)
+
+def test_random_weighted_graph_neighbor_1():
+	"""
+	check that the weights on the graph are changed properly
+	"""
+	random.seed(1)
+	WS = get_figure_2_weighted_synergy_graph()
+	G = WS.graph
+
+	# Based on this seed, the first change increases the edge weight
+	H = random_weighted_graph_neighbor(G)
+	assert H[0][0]['weight'] == 6
+
+	# Based on this seed, the second change decreases the edge weight
+	H = random_weighted_graph_neighbor(G)
+	assert H[1][1]['weight'] == 3
+
+	initial_nodes = [n for n in G]
+	initial_edges = [e for e in G.edges]
+
+	new_nodes = [n for n in H]
+	new_edges = [e for e in H.edges]
+
+	assert new_edges == initial_edges
+	assert new_nodes == initial_nodes
+
