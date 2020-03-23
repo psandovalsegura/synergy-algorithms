d3831cbdba6294e6653f3f947c9384b389367999
Pedro Sandoval <psandovalsegura@gmail.com>
2020-03-23 15:32:58 -0400

    Add random role assignment neighbor functions and add log likelihood by role

--- src/role_assignment.py
+++ src/role_assignment.py
@@ -1,7 +1,64 @@
+import copy
+import random
 import itertools
 from scipy.special import comb
-from src.weighted_synergy_graph import WeightedSynergyGraph
+from src.weighted_synergy_graph import WeightedSynergyGraph, random_weighted_graph_neighbor
 from src.normal_distribution import NormalDistribution
+
+def learn_weighted_synergy_graph(num_agents, R, T, display=True):
+	"""
+	num_agents is the number of agents
+	R is a list of roles
+	T is a list of training examples [(pi, V(pi))]
+	"""
+	nearest_neighbors = 3
+	rewire_prob = 0.30
+	G = nx.generators.random_graphs.connected_watts_strogatz_graph(num_agents, nearest_neighbors, rewire_prob)
+	C = estimate_capability_by_role(G, R, T)
+
+	# Create initial synergy graph
+	initial_wsgraph = WeightedSynergyGraph(G, C)
+	initial_pi = create_random_role_assignment(num_agents, R)
+
+	value_function = lambda x: log_likelihood_by_role(x, T, weight_fn)
+	def random_neighbor(s):
+		G_prime = random_weighted_graph_neighbor(s.graph)
+		C_prime = estimate_capability_by_role(G_prime, R, T, weight_fn)
+		return WeightedSynergyGraph(G_prime, C_prime)
+
+	final_sgraph, final_value, sgraphs, values = annealing(initial_wsgraph, value_function, random_neighbor, debug=False, maxsteps=k_max)
+
+	if display:
+		# plot num_graphs and the final graph
+		num_graphs = 5
+		num_steps = len(sgraphs)
+		step_size = int(num_steps / num_graphs) if (num_steps / num_graphs) >= 1 else 1
+		for i, sgraph_index in enumerate(range(0, num_steps, step_size)):
+			title = f"Step {sgraph_index} ({values[sgraph_index]:.2f})"
+			if i <= num_graphs: sgraphs[sgraph_index].display(1, num_graphs + 1, i + 1, title=title)
+		final_title = f"Final {num_steps} ({final_value:.2f})"
+		sgraphs[-1].display(1, num_graphs + 1, num_graphs + 1, title=final_title)
+		plt.show()
+
+	return final_sgraph, final_value, sgraphs, values
+
+def estimate_capability_by_role(G, R, T):
+	pass
+
+def log_likelihood_by_role(WS, T, weight_fn):
+	"""
+	Compute the log-likelihood of the training data 
+	given the WeightedSynergyGraph
+
+	WS is a WeightedSynergyGraph
+	T is a list of training examples
+	"""
+	likelihood = 0
+	for example in T:
+		pi, V = example
+		synergy_by_role = synergy_by_role_policy(WS, pi, weight_fn)
+		likelihood += synergy_by_role.logpdf(V).item()
+	return likelihood
 
 def synergy_by_role_policy(WS, pi, weight_fn):
 	"""
@@ -29,3 +86,31 @@ def pairwise_synergy_by_role_policy(WS, weight_fn, a, b, r_a, r_b):
 	b_capability_at_r_b = WS.get_capability(b, r_b)
 	w = weight_fn(distance)
 	return w * (a_capability_at_r_a + b_capability_at_r_b)
+
+def create_random_role_assignment(num_agents, R):
+	"""
+	num_agents is the number of agents
+	R is a list of roles (starting at index 0)
+	"""
+	pi = dict()
+	agents = list(range(num_agents))
+	for role in R:
+		pi[role] = random.choice(agents)
+
+	return pi
+
+def random_role_assignment_neighbor(pi, num_agents, R):
+	"""
+	pi is the current role assignment policy
+	num_agents is the number of agents
+	R is a list of roles (starting at index 0)
+	"""
+	pi_prime = copy.deepcopy(pi)
+	role_to_modify = random.choice(R)
+
+	# Select new agent from set which excludes the previous agent assignment
+	agents = list(set(range(num_agents)) - set([pi_prime[role_to_modify]]))
+	pi_prime[role_to_modify] = random.choice(agents)
+	return pi_prime
+
+
--- tests/test_role_assignment.py
+++ tests/test_role_assignment.py
@@ -37,3 +37,72 @@ def test_synergy_by_role_policy():
 	pi[1] = 2
 
 	assert synergy_by_role_policy(WS, pi, weight_fn_reciprocal) == (1/5) * NormalDistribution(5,3)
+
+def test_create_random_role_assignment_1():
+	"""
+	check that the role assignment fills all the roles
+	Note: roles must be indexed starting at 0
+	"""
+	pi = create_random_role_assignment(6, [0,1,2,3])
+	assert len(pi.keys()) == 4
+	for key in pi.keys():
+		assert pi[key] <= 6
+
+
+def test_create_random_role_assignment_2():
+	"""
+	check that the role assignment fills all the roles
+	Note: roles must be indexed starting at 0
+	"""
+	pi = create_random_role_assignment(10, [0,1,2,3])
+	assert len(pi.keys()) == 4
+	for key in pi.keys():
+		assert pi[key] <= 10
+
+def test_create_random_role_assignment_3():
+	"""
+	check that the role assignment fills all the roles
+	Note: roles must be indexed starting at 0
+	"""
+	pi = create_random_role_assignment(2, [0,1,2,3,4,5])
+	assert len(pi.keys()) == 6
+	for key in pi.keys():
+		assert pi[key] <= 2
+
+def test_random_role_assignment_neighbor():
+	"""
+	check that only one change is made to the role assignment policy
+	"""
+	num_agents = 10
+	R = [0,1,2,3]
+	pi = create_random_role_assignment(num_agents, R)
+	pi_prime = random_role_assignment_neighbor(pi, num_agents, R)
+
+	assert len(pi.keys()) == len(pi_prime.keys())
+	num_changes = 0
+	for key in pi_prime.keys():
+		if pi_prime[key] != pi[key]:
+			num_changes += 1
+
+	assert num_changes == 1
+
+def test_log_likelihood_by_role():
+	"""
+	check that the log likelihood of the training data is correct 
+	"""
+	WS = get_figure_2_weighted_synergy_graph()
+
+	pi = dict()
+	pi[0] = 1
+	pi[1] = 2
+	v = 1.5
+	T = [(pi, v)]
+
+	expected_synergy = (1/5) * NormalDistribution(5,3)
+	expected_log_likelihood = -0.5 * np.log(2 * np.pi * expected_synergy.variance) - 0.5 * ((v - expected_synergy.mean)**2/(expected_synergy.variance))
+
+	assert np.round(log_likelihood_by_role(WS, T, weight_fn_reciprocal), 3) == np.round(expected_log_likelihood, 3)
+
+
+
+
